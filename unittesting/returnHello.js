29e036f26e66b6a5df340c94918b4c122c614194
Rhys Adams <rhys@therhys.co.uk>
2023-02-06 10:48:31 +0000

    Fix unittest languages

--- unittesting/returnHello.js
+++ unittesting/returnHello.js
@@ -3,10 +3,14 @@ const generateHelloString = (language = "english") => {
   switch (language) {
     case "english":
       return "Hello";
-      break;
     case "gurmukhi":
       return "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ";
-      break;
+    case "italian":
+      return "Bonjourno";
+    case "german":
+      return "Guten Tag";
+    case "french":
+      return "Bonjour";
     default:
       throw RangeError(`Unknown language ${language}`);
   }
