5f328518f31c681d9551aba5f08ff44016205795
Rhys Adams <rhys@therhys.co.uk>
2023-02-06 10:40:12 +0000

    Fix code in /mashup.

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,3 @@
-const a = 'b';
-console.log('Hello this is mashup js')
+console.log("Hello this is mashup js");
 
-if (myStatement = false)
-
-{
-  console.log('This is mashup' + 'strings'); if (true == false) { }
-}
+console.log("This is mashup" + "strings");
